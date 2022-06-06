83d8f773c98ea30a272138d455ba53b79a841817
taylorelley <48809387+taylorelley@users.noreply.github.com>
2022-06-07 09:15:21 +1200

    Add files via upload

---
+++ functions/general/nix_ubuntu2004_banner.sh
@@ -0,0 +1,15 @@
+BANR()
+{
+	echo ""
+	echo ""
+	echo "     ######################################################## "
+	echo "     #  CIS Linux Build Kit for Ubuntu Linux 20.04 LTS      # "
+	echo "     #  This Linux Build Kit works in conjunction with the  # "
+	echo "     #  CIS Ubuntu Linux 20.04 LTS Benchmark v1.1.0         # "
+	echo "     #  This script is the property of:                     # "
+	echo "     #  Center for Internet Security (CIS)                  # "
+	echo "     ######################################################## "
+	echo ""
+	echo ""
+}
+# End of print bannor to Screen
